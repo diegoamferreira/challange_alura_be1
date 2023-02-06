b0a255dfbc341cb7876b2e5bda6b88d95688e7f0
Diego Ferreira <diego.cpm@gmail.com>
2023-02-05 22:09:21 -0300

    fix: urls

--- challange_alura_be1/urls.py
+++ challange_alura_be1/urls.py
@@ -20,14 +20,17 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('aluraflix.urls')),
+    path('', include('aluraflix.urls')),
     path('users/', include('users.urls')),
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token-auth/', obtain_auth_token),
+]
+urlpatterns += [
     path('swagger/<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
